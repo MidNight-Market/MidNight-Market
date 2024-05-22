97b632512246cb3360cc91fb00e2a6f312013e14
ehdwo13 <ehdwo13@gmail.com>
2024-05-22 10:16:55 +0900

    security option update

--- src/main/java/com/project/www/controller/CustomerController.java
+++ src/main/java/com/project/www/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.project.www.controller;
 
+import com.project.www.domain.CustomerVO;
 import com.project.www.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -12,4 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomerController {
     private final CustomerService csv;
+
+    @GetMapping("/register")
+    public void register() {}
+
+    @PostMapping("/register")
+    public String register(CustomerVO cvo){
+
+        return null;
+    }
 }
