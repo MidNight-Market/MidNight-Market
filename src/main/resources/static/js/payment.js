5d938a9f4695158cf66e871b473528f6ad61f329
oco0217 <oco0217@gmail.com>
2024-05-30 16:19:06 +0900

    세큐리티 하기전 병합 커밋

--- src/main/resources/static/js/payment.js
+++ src/main/resources/static/js/payment.js
@@ -1,47 +1,49 @@
 
-
 document.getElementById('purchaseButton').addEventListener('click', ()=> {
     let IMP = window.IMP; // Iamport 객체 초기화
+    const merchant_uid = 'merchant_uid' + new Date().getTime();
     IMP.init('imp53054186'); // 가맹점 식별코드
 
-    //'merchant_uid' + new Date().getTime(),
-    IMP.request_pay({
-        pg: 'html5_inicis',
-        pay_method: 'card',
-        merchant_uid: '1111',
-        name: `${productDTO.productVO.name}`,
-        amount:Number(`${document.getElementById('productPrice').innerText.replace(/[,원]/g,"")}`),
-        buyer_email: 'email',
-        buyer_name: '윤찬웅',
-        buyer_tel: '010-1234-5678',
-        buyer_addr: 'address',
-    }, function(rsp) {
-        console.log(rsp);
+    //결제금액 사전등록
+    $.ajax({
+        url: "https://api.iamport.kr/payments/prepare",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({
+            merchant_uid: merchant_uid, // 가맹점 주문번호
+            amount:Number(`${document.getElementById('productPrice').innerText.replace(/[,원]/g,"")}`), // 결제 예정금액
+        }),
+        success: function(response) {
+            // 성공 시 실행할 코드
 
-        //결제 검증
-        $.ajax({
-            type : 'POST',
-            url : "/verifyIamprot/" + rsp.imp_uid
-        }).done(function (data){
+            IMP.request_pay({
+                pg: 'html5_inicis',
+                pay_method: 'card',
+                merchant_uid: merchant_uid,
+                name: `주문명: ${productDTO.productVO.name}`,
+                amount:Number(`${document.getElementById('productPrice').innerText.replace(/[,원]/g,"")}`),
+                buyer_email: 'email',
+                buyer_name: 'nick_name',
+                buyer_tel: 'tel',
+                buyer_addr: 'address',
+                buyer_postcode: '123-456'
+            }, function(rsp) {
+                if (rsp.success) {
+                    alert('결제가 완료되었습니다.');
+                    window.location.href = '/';
+                    // 결제 성공 후 처리
+                } else {
+                    alert('결제에 실패하였습니다. 에러 내용: ' + rsp.error_msg);
+                    // 결제 실패 후 처리
+                }
+            });
 
-            console.log(data +"gdgddgd");
-
-            if(rsp.paid_amount == data.response.amount){
-                alert("결제 검증 완료");
-            }else{
-                alert("결제 검증 실패");
-            }
-
-        });
+            console.log(response);
+        },
+        error: function(xhr, status, error) {
+            // 실패 시 실행할 코드
+            console.error(xhr, status, error+"사전등록과 비교실패");
+        }
     });
-});
 
-//결제 성공 여부
-// if (rsp.success) {
-//     alert('결제가 완료되었습니다.');
-//     window.location.href = '/';
-//     // 결제 성공 후 처리
-// } else {
-//     alert('결제에 실패하였습니다. 에러 내용: ' + rsp.error_msg);
-//     // 결제 실패 후 처리
-// }
+});
