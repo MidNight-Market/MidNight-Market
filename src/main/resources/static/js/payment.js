c17a716ce894bfe51d2c43958d1d9cfd66252d27
havanara <dlskfk1414@gmail.com>
2024-06-14 18:04:59 +0900

    orders 배송지(payment.js)

--- src/main/resources/static/js/payment.js
+++ src/main/resources/static/js/payment.js
@@ -82,3 +82,78 @@ document.getElementById('purchaseButton').addEventListener('click', (e)=> {
         }
     });
 });
+
+
+
+// 배송지 등록
+function daumPost() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분
+
+            // 각 주소의 노출 규칙에 따라 주소를 조합
+            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가짐
+            var addr = ''; // 주소 변수
+            var extraAddr = ''; // 참고항목 변수
+
+            //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져옴
+            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else { // 사용자가 지번 주소를 선택했을 경우
+                addr = data.jibunAddress;
+            }
+
+            // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합
+            if(data.userSelectedType === 'R'){
+                // 법정동명이 있을 경우 추가 (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝남
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraAddr += data.bname;
+                }
+                // 건물명이 있고 공동주택일 경우 추가
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+            } else {
+                document.getElementById("sample6_extraAddress").value = '';
+            }
+
+            // 값 넣기
+            document.getElementById('sample6_postcode').value = data.zonecode; // 우편번호
+            document.getElementById("sample6_address").value = addr; // 주소
+
+            // 커서 이동
+            document.getElementById("sample6_detailAddress").focus();
+        }
+    }).open();
+}
+
+// 추가된 배송지 db저장
+document.querySelector('.basket6').addEventListener('click',()=>{
+    let code = document.getElementById('sample6_postcode').value;
+    let add1 = document.getElementById('sample6_address').value;
+    let add2 = document.getElementById('sample6_detailAddress').value;
+    document.getElementById('address').value = code+"/"+add1+"/"+add2;
+});
+
+// 배송지 추가 모달
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.addrAddBtn').addEventListener('click', function() {
+        var addrModal = document.getElementById('addrModal');
+        addrModal.style.display = 'block';
+    });
+
+    // 모달 닫기 버튼
+    document.querySelector('.addrClose').addEventListener('click', function() {
+        var addrModal = document.getElementById('addrModal');
+        addrModal.style.display = 'none';
+    });
+
+    // 모달 영역 외 다른 구간 클릭 시 모달 닫음
+    window.addEventListener('click', function(e) {
+        var addrModal = document.getElementById('addrModal');
+        if (e.target == addrModal) {
+            addrModal.style.display = 'none';
+        }
+    });
+});
