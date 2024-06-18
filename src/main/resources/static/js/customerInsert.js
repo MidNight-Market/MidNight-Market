1b7fe20613b689f24c57fc8fade96f4177e284da
ehdwo13 <ehdwo13@gmail.com>
2024-06-18 22:21:34 +0900

    customer register validation complete

--- src/main/resources/static/js/customerInsert.js
+++ src/main/resources/static/js/customerInsert.js
@@ -1,3 +1,10 @@
+let duplicateIdCheck = false;
+let duplicateNicknameCheck = false;
+let duplicateIdCheckStatus = false;
+let duplicateNickNameCheckStatus = false;
+let confirmEmailCheck = false;
+let confirmPasswordCheck = false;
+let confirmRepeatPasswordCheck = false;
 function changeVal(){
     let result ={};
     const emailDomain = document.getElementById('emailDomain');
@@ -51,6 +58,7 @@ document.getElementById('confirmBtn').addEventListener('click', ()=>{
         console.log(result);
         if(result === "1"){
             alert("인증번호 확인 완료.");
+            confirmEmailCheck = true;
             document.getElementById('mail_number').style.display="none";
             document.getElementById('checkBtn').style.display="none";
             document.getElementById('sendBtn').style.display="none";
@@ -93,6 +101,7 @@ function startTimer(count, display) {
 }
 
 document.getElementById('checkBtn').addEventListener('click', ()=>{
+    duplicateIdCheck = true;
     let email = document.getElementById('idF').value+"@"+document.getElementById('idB').value;
     if(validateEmail(email)){
         checkEmail(email).then(result =>{
@@ -101,6 +110,7 @@ document.getElementById('checkBtn').addEventListener('click', ()=>{
                 document.getElementById("customerEmail").value = '';
             }else{
                 alert("사용 가능한 이메일입니다. ");
+                duplicateIdCheckStatus = true;
                 document.getElementById('sendBtn').style.display = '';
                 document.getElementById('customerEmail').value = document.getElementById('idF').value+"@"+document.getElementById('idB').value;
                 console.log(document.getElementById('customerEmail').value)
@@ -139,6 +149,7 @@ document.getElementById('p').addEventListener("change", ()=> {
         document.getElementById('validText').innerText = " 비밀번호는 8글자 이상, 영문, 숫자, 특수문자를 포함해야 합니다. "
     }else{
         document.getElementById('validText').style.display = 'none';
+        confirmPasswordCheck = true;
     }
 })
 document.getElementById('pc').addEventListener("change", ()=>{
@@ -150,12 +161,14 @@ document.getElementById('pc').addEventListener("change", ()=>{
         document.getElementById('checkPw').innerText = "입력하신 패스워드가 동일하지 않습니다. "
     }else{
         document.getElementById('checkPw').style.display = 'none';
+        confirmRepeatPasswordCheck = true;
     }
 })
 function validPW (pw) {
     return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(pw);
 }
 document.getElementById('checkNick').addEventListener('click', ()=>{
+    duplicateNicknameCheck = true;
     let nickName = document.getElementById('n').value;
     checkNick(nickName).then(result=>{
         if(result === "1"){
@@ -163,6 +176,7 @@ document.getElementById('checkNick').addEventListener('click', ()=>{
             document.getElementById("n").value = '';
         }else{
             alert("사용가능한 닉네임입니다. ");
+            duplicateNickNameCheckStatus = true;
         }
     })
 
@@ -197,7 +211,7 @@ function chkAll(isChecked) {
     });
     // 최소 두 개의 체크박스가 선택되었는지 확인하여 가입하기 버튼을 활성화
     const checkedCount = Array.from(pchkBoxes).filter((checkbox) => checkbox.checked).length;
-    joinBtn1.disabled = checkedCount < 2;
+    joinBtn.disabled = checkedCount < 2;
 }
 
 // chkAll 체크박스에 이벤트 리스너 추가
@@ -220,3 +234,27 @@ function agreement(){
 function disagreement(){
     alert("이용약관 필수사항에 동의하시지 않으셨습니다.")
 }
+document.getElementById('joinBtn1').addEventListener('click', (e)=>{
+    if(duplicateIdCheck == false){
+        e.preventDefault();
+        alert("아이디 중복체크를 해주세요. ");
+    }else if(duplicateNicknameCheck == false){
+        e.preventDefault();
+        alert("닉네임 중복체크를 해주세요. ")
+    }else if(duplicateIdCheckStatus == false){
+        e.preventDefault();
+        alert("아이디 값이 중복입니다. 다시 체크해주세요. ")
+    }else if(duplicateNicknameCheck == false){
+        e.preventDefault();
+        alert("닉네임 값이 중복입니다. 다시 체크해주세요. ")
+    }else if(confirmEmailCheck == false){
+        e.preventDefault();
+        alert("이메일 인증을 통과하지 못했습니다. 다시 시도해주세요. ")
+    }else if(confirmPasswordCheck == false){
+        e.preventDefault();
+        alert("비밀번호가 입력형식에 맞지 않습니다.\n비밀번호는 8글자 이상, 영문, 숫자, 특수문자를 포함해야 합니다.")
+    }else if(confirmRepeatPasswordCheck == false){
+        e.preventDefault();
+        alert("비밀번호 확인란이 동일하지 않습니다. 다시 입력해주세요. ")
+    }
+})
