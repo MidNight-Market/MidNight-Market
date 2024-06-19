55e858436fc4f21efb8f75ea622806e752ce3727
ehdwo13 <ehdwo13@gmail.com>
2024-06-19 15:51:48 +0900

    recentSearch

--- src/main/resources/static/js/sidebar.js
+++ src/main/resources/static/js/sidebar.js
@@ -1,7 +1,6 @@
 function openPopup() {
     window.open('/popup', 'PopupWindow', 'width=700,height=700,scrollbars=yes,left=700,top=230, scrollbars = yes');
 }
-console.log(role);
 document.getElementById('bannerBasket').addEventListener('click', ()=>{
     if(role != "role_user" || role == null){
         if(confirm("구매자만 사용가능한 서비스 입니다. 로그인 하시겠습니까? ")){
